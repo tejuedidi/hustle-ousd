22c287d166062a52814f523f4b2922185aa07cb2
Tejaswini Edidi <tejuedidi@gmail.com>
2025-06-17 11:47:40 -0700

    added lab2.py

---
+++ lab2.py
@@ -0,0 +1,27 @@
+# Lab 2 - Teju Edidi
+
+# Step 1: Name Variable
+name = "Tejaswini Edidi"
+
+# Step 2: Nickname Varibale
+nickname = "Teju"
+
+# Step 3: Age Variable
+age = 23
+
+# Step 4: Boolean Variable 
+used_before = True
+
+# Step 5: List Variable 
+hobbies = ["beading", "painting", "TV", "jewelry", "coding"] # list of strings
+
+# Step 6: Dictionary Variable
+my_favs = {'beading':'flowers','movie': 'OSO', 'cartoon':["fairly odd parents", "steven universe", "regular show", "TAWOG"]}
+
+# Step 7: Set Variable
+teachers = {"parker b", "parker bo", "mia"}
+# print(teachers)
+
+# Step 7: Tuple Variable
+teachers = ("parker b", "parker b", "mia")
+print(teachers)
