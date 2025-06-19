d1279eabbe52f82aab73959d70a9213633258b7f
Tejaswini Edidi <tejuedidi@gmail.com>
2025-06-18 17:14:13 -0700

    added lab2

--- lab2.py
+++ lab2.py
@@ -1,33 +1,21 @@
-# Lab 2 - Teju Edidi 
+# Lab 2: Teju Edidi
 
 # Step 1: Name Variable
 name = "Tejaswini Edidi"
 
-# Step 2: Nickname Varibale
+# Step 2: Nickname Variable
 nickname = "Teju"
 
 # Step 3: Age Variable
+# integer = whole num, float = decimal
 age = 23
 
-# Step 4: Boolean Variable 
+# Step 4: Boolean Variable (True or False)
 used_before = True
 
-# Step 5: List Variable 
-hobbies = ["beading", "painting", "TV", "jewelry", "coding"] # list of strings
+# Step 5: List Data Strucutre
+hobbies = ["paint", "film photo", "TV", "animals"]
 
-# Step 6: Dictionary Variable
-my_favs = {'beading':'flowers','movie': 'OSO', 'cartoon':["fairly odd parents", "steven universe", "regular show", "TAWOG"]}
+# Step 6: Dictionary (key : value) pairs
+my_favs = {"food":["sushi", "tacos", "pizza", "boba"], "soda":"diet coke", "animal":"dog"}
 
-# Step 7: Set Variable
-teachers = {"mia", "parker b", "parker d"}
-# print(teachers)
-
-# Step 8: Tuple Variable
-teachers1 = ("parker b", "parker b", "apple", "mia")
-
-print(name + ' ' + nickname)
-print(age)
-print(hobbies)
-print(my_favs)
-print(teachers)
-print(teachers1)
