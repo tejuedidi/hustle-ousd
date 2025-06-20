551a47ee57c4ac5051666405961a240760933e37
Tejaswini Edidi <tejuedidi@gmail.com>
2025-06-20 09:27:41 -0700

    lab upload

---
+++ lab2.py
@@ -0,0 +1,35 @@
+# Lab 2: Teju Edidi
+
+# Step 1: Name Variable
+name = "Tejaswini Edidi"
+
+# Step 2: Nickname Variable
+nickname = "Teju" 
+
+# Step 3: Age Variable
+# numeric data type - int or integer (whole number)
+# float or floating point (decimal)
+age = 23
+
+# Step 4: Boolean Variable
+# True or False
+used_before = True
+
+# Step 5: List Data Structure 
+# [ ]
+hobbies = ["painting", "cat sitting", "craft", "TV"] # list of strings
+
+# Step 6: Dictionary Data Structure
+# (key : value) pairs 
+# { }
+my_favs = {"cartoon": "steven universe", "food": ["sushi", "pizza", "burgers", "fries"], "flower":"sunflower"}
+
+# Step 7: Set Data Structure
+# collection of items - unique (no duplicates) unordered
+teachers = {"sydney", "sydney", "parker", "cris", "teju"}
+print(teachers)
+
+# Step 8: Tuple Data Structure
+# collection of items - unchangeable ordered can have duplicates ( )
+teachers1 = ("sydney",  "parker", "cris", "sydney", "teju")
+print(teachers1)
