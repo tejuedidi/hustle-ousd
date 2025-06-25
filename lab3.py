c4800a46239b0b16dc99890b37a0a7a44e96457d
Tejaswini Edidi <tejuedidi@gmail.com>
2025-06-25 14:28:17 -0700

    added lab3

--- lab3.py
+++ lab3.py
@@ -3,25 +3,23 @@
 # Task 1: Strings
 food = "cheese cake" # step1
         # 012345678910
-# print("The first 3 characters: ", food[0:3]) # 2
-# print("The last 3 characters: ", food[8:]) # 3
-# first_last = food[0] + food[-1]
-# print("The first characters + last character:", first_last)
-# split_str = food.split()
-# print("The split method", split_str)
-# print("The join method", "".join(split_str))
+# [start:end]              
+print("The first 3 characters: ", food[0:3]) # 2
+print("The last 3 characters: ", food[-3:]) # 3
+first_last = food[0] + food[-1]
+print("The first characters + last character:", first_last)
+split_str = food.split()
+print("The split method", split_str)
+print("The join method", "##".join(split_str))
 
 
 # Task 2: List [ ]
-number_list = [1,20,2,3,4,5,6,7,8,8] #1
+number_list = [1,-20,2,31,4,5,6,7,8,8] #1
 number_list.append(9) #2
 number_list.insert(3, 100) #3
 number_list.pop() #4
-number_list.remove(20) # 5
-# print(number_list)
-# print(number_list[0:3])
-
-
+number_list.remove(-20) # 5
+print(number_list)
 
 
 
@@ -41,13 +39,9 @@ number_list.remove(20) # 5
 
 
 # Task 3: Working with Dictionaries (key : value)
-books = {"author1" : "Four Mice Deep in the Jungle", 
+books = {"Elisabetta Dami" : "Four Mice Deep in the Jungle", 
 "Selina" : "Working in Python", 
 "Rabindranath Tagore": "Gitanjali"} # 1
 print(books.keys()) #2
 print(books.values()) #3
-print(books.get("Selina")) # 4
-books.pop("Rabindranath Tagore")
-print(books)
-del books["Elisabetta Dami"]
-print(books)
+print(books.get("Elisabetta Dami")) # 4
